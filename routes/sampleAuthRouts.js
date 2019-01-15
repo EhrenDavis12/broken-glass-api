75f645ab2f01b579733638af5ccfe09b9843f242
Ehren Davis <ehren.davis12@gmail.com>
2019-01-14 18:59:08 -0700

    working Private API sample

--- routes/sampleAuthRouts.js
+++ routes/sampleAuthRouts.js
@@ -1,7 +1,15 @@
-module.exports = function(app) {
+module.exports = function(app, checkJwt) {
   app.get("/api/v1/public", function(req, res) {
     try {
-      res.json("Hello from a public API");
+      res.status(200).json("Hello from a public API");
+    } catch (err) {
+      res.status(400).json("Invalid request");
+    }
+  });
+
+  app.get("/api/v1/private", checkJwt, function(req, res) {
+    try {
+      res.status(200).json("Hello from a private API");
     } catch (err) {
       res.status(400).json("Invalid request");
     }
