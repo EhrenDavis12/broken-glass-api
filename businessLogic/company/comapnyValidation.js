408097da3bf3af609f0fb94fdeb1000c5b47460c
Olevina <olevina@ymail.com>
2019-01-15 21:46:22 -0700

    POST Olga needs debug

--- businessLogic/company/comapnyValidation.js
+++ businessLogic/company/comapnyValidation.js
@@ -1,11 +1,5 @@
  var db = require("../../models");
 
-// module.exports = function () {
-
-// return  function test1(){  return "company found";}
-
-// };
-
 
 
 const ComVal = function() {
@@ -13,33 +7,70 @@ const ComVal = function() {
       return "test1 is working!";
     };
 
-    this.companyCheck = (pid) => {
+    this.companyCheck = (body,pres, insertCompany) => {
         //1. query company id to see if it exists. 
         //if it exists return true
         db.Company.findAll({
           where:{
-            id: pid 
+            id: body.googleMapsId 
           }
         }).then(function (res) {
           console.log("results from query: ");
-          console.log(res[0].id);
-          if(res.length>0){
+          // console.log(res[0].id);
+         
+          if(res.length>=0){
+            console.log("already exist");
             return true;
           }
           else{
             //in this case we need to add the company data to our database
-            return false;
+           
+           db.Company.create({
+            id: body.id,
+            googleMapId: body.company.googleMapId,
+            companyName: body.company.category,
+            averageRating: body.company.averageRating,
+            reviewCount: body.company.reviewCount,
+
+           }).then(function (res1){
+              insertCompany(body, res)
+           })
+
+           return false;
           }
+
         });
-        return "companyCheck is working! this is my id: " + id;
+        // return "companyCheck is working!" ;
 
     };
 
-    this.jobCheck(JobDescription) = (pid) => {
-    //in here check to see if the job description already exists in the database. 
-    //if not add to jobType table. if it does get id and pass to review
-    
-    }
+    this.insertReview= (body, res) => {
+
+        
+      db.Review.create({
+        uuid: body.review.uuid,
+        userId: body.review.userId,
+        shiftPayComent: body.review.shiftPayComent,
+        shiftPayRating: body.review.shiftPayRating,
+        managementComment: body.review.managementComment,
+        managementRating: body.review.managementRating,
+        busyComment: body.review.busyComment,
+        busyRating: body.review.busyRating,
+        customerComment: body.review.customerComment,
+        customerRating: body.review.customerRating,
+        overallComment: body.review.overallComment,
+        overallRating: body.review.overallRating,
+        companyId: body.review.companyId,
+        JobTypeId: body.review.JobTypeId,
+        PayTypeId:body.review.PayTypeId
+
+
+       }).then(function (res1){
+          res.status(200).json("Review has been added!")
+       })
+
+    }; //end comapny insert
+
   };
   
   module.exports = ComVal;
