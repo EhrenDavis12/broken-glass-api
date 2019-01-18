7618828b2ac4d4e0bd28141da11f3cd864227345
Ehren Davis <ehren.davis12@gmail.com>
2019-01-17 19:12:04 -0700

    Create the auth0 admin endpoint as well as cleaned up some code

--- routes/sampleAuthRouts.js
+++ routes/sampleAuthRouts.js
@@ -1,4 +1,6 @@
-module.exports = function(app, checkJwt) {
+const { checkJwt, checkRole, checkScope } = require("./auth0/auth0");
+
+module.exports = function(app) {
   app.get("/api/v1/public", function(req, res) {
     try {
       res.status(200).json("Hello from a public API");
@@ -10,6 +12,25 @@ module.exports = function(app, checkJwt) {
   app.get("/api/v1/private", checkJwt, function(req, res) {
     try {
       res.status(200).json("Hello from a private API");
+    } catch (err) {
+      res.status(400).json("Invalid request");
+    }
+  });
+
+  app.get("/api/v1/review", checkJwt, checkScope(["read:review"]), function(
+    req,
+    res
+  ) {
+    try {
+      res.status(200).json("Hello you may read private reviews");
+    } catch (err) {
+      res.status(400).json("Invalid request");
+    }
+  });
+
+  app.get("/api/v1/admin", checkJwt, checkRole("admin"), function(req, res) {
+    try {
+      res.status(200).json("Hello from an admin API");
     } catch (err) {
       res.status(400).json("Invalid request");
     }
