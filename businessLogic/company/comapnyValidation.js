985a67e7e73cb27e67fee00d73510c618e9a79c4
Olevina <olevina@ymail.com>
2019-01-19 14:24:15 -0700

    readyAPI

--- businessLogic/company/comapnyValidation.js
+++ businessLogic/company/comapnyValidation.js
@@ -1,80 +1,67 @@
- var db = require("../../models");
+var db = require("../../models");
 
-
-
-const ComVal = function() {
-    this.basicMeg = () => {
-      return "test1 is working!";
-    };
-
-    insertReview= (body, res) => {
-
-        
-      db.Review.create({
-        // uuid: body.review.uuid,
-        userId: body.review.userId,
-        shiftPayComent: body.review.shiftPayComent,
-        shiftPayRating: body.review.shiftPayRating,
-        managementComment: body.review.managementComment,
-        managementRating: body.review.managementRating,
-        busyComment: body.review.busyComment,
-        busyRating: body.review.busyRating,
-        customerComment: body.review.customerComment,
-        customerRating: body.review.customerRating,
-        overallComment: body.review.overallComment,
-        overallRating: body.review.overallRating,
-        companyId: body.review.companyId,
-        JobTypeId: body.review.JobTypeId,
-        PayTypeId:body.review.PayTypeId
-
-
-       }).then(function (res1){
-
-       })
-
-    }; 
-
-    this.companyCheck = (body,pres) => {
-
-        db.Company.findAll({
-          where:{
-            googleMapId: body.company.googleMapId 
-          }
-        }).then(function (res) {
-          console.log("results from query: ");
-          console.log(res.length);
-         
-          if(res.length>=1){
-            console.log("already exist");
-            insertReview(body, pres);
-            return true;
-          }
-          else{
-            //in this case we need to add the company data to our database
-           
-           db.Company.create({
-            id: body.company.id,
-            googleMapId: body.company.googleMapId,
-            companyName: body.company.category,
-            averageRating: body.company.averageRating,
-            reviewCount: body.company.reviewCount,
-
-           }).then(function (pres){
-              insertReview(body, pres);
-              console.log("company has been added!");
-              
-           })
-
-           return false;
-          }
-
-        });
-
-
-    };
-
- 
-
+const ComVal = function () {
+  this.basicMeg = () => {
+    return "test1 is working!";
   };
-  
-  module.exports = ComVal;
+
+  insertReview = (body, res) => {
+
+    db.Review.create({
+      // uuid: body.review.uuid,
+      userId: body.review.userId,
+      shiftPayComent: body.review.shiftPayComent,
+      shiftPayRating: body.review.shiftPayRating,
+      managementComment: body.review.managementComment,
+      managementRating: body.review.managementRating,
+      busyComment: body.review.busyComment,
+      busyRating: body.review.busyRating,
+      customerComment: body.review.customerComment,
+      customerRating: body.review.customerRating,
+      overallComment: body.review.overallComment,
+      overallRating: body.review.overallRating,
+      CompanyId: body.company.id,
+      JobTypeId: body.review.JobTypeId,
+      PayTypeId: body.review.PayTypeId
+
+    }).then(function (res1) {
+    })
+  };
+
+  this.companyCheck = (body, pres) => {
+
+    db.Company.findAll({
+      where: {
+        googleMapId: body.company.googleMapId
+      }
+    }).then(function (res) {
+      console.log("results from query: ");
+      console.log(res.length);
+
+      if (res.length >= 1) {
+        console.log("already exist");
+        insertReview(body, pres);
+        return true;
+      }
+      else {
+        //in this case we need to add the company data to our database
+
+        db.Company.create({
+          id: body.company.id,
+          googleMapId: body.company.googleMapId,
+          companyName: body.company.category,
+          averageRating: body.company.averageRating,
+          reviewCount: body.company.reviewCount,
+
+        }).then(function (pres) {
+          insertReview(body, pres);
+          console.log("company has been added!");
+        })
+        return false;
+      }
+
+    });
+  };
+};
+
+module.exports = ComVal;
